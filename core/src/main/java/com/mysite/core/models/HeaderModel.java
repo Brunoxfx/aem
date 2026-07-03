5c19df9ba2b94d4326043a46f3e1f3ee73e6baeb
Zinorblatant-cpu <leonardolopesolieira851@gmail.com>
2026-07-03 13:43:57 -0300

    feat: create Java model for flexible header middle titles

--- core/src/main/java/com/mysite/core/models/HeaderModel.java
+++ core/src/main/java/com/mysite/core/models/HeaderModel.java
@@ -1,9 +1,9 @@
 package com.mysite.core.models;
 
-import org.apache.sling.models.annotations.Model;
-import org.apache.sling.models.annotations.DefaultInjectionStrategy;
-import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
 import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
 
 @Model(
     adaptables = Resource.class,
@@ -14,7 +14,14 @@ public class HeaderModel {
     @ValueMapValue
     private String[] menuItems;
 
+    @ValueMapValue
+    private String[] textButtonHeader;
+
     public String[] getMenuItems() {
         return menuItems;
+    }
+
+    public String[] getTextButtonHeader() {
+        return textButtonHeader;
     }
 }
